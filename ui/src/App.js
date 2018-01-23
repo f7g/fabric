70852f479205f3c4e2b28bef49452e29f0a944e1
f7g <fikrigha@gmail.com>
2018-01-22 19:19:05 -0500

    Implement simple Fabric component

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Fabric
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { Button, ButtonType } from 'office-ui-fabric-react/lib/Button';
+import { Dialog, DialogType, DialogFooter } from 'office-ui-fabric-react/lib/Dialog';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fabric className="App">
+        <img src={logo} className="App-logo" alt="logo" style={{marginTop: '2em'}}/>
+      </Fabric>
     );
   }
 }
 
 export default App;
+
+
